5ee0febe8cb894c4bd8f57ba02875a8c9756c60f
Erdem Ozgen <m.erdemozgen@gmail.com>
2021-11-01 13:34:15 +0300

    Add chapter fix

---
+++ src/eloquentJavascript/Chapter0.js
@@ -0,0 +1,44 @@
+//Example JS
+let total = 0, count = 1;
+while (count <= 10) {
+  total += count;
+  count += 1;
+}
+console.log(total);
+// → 55
+
+function range(start, end, step) {
+  if (step == null)
+    step = 1;
+  var array = [];
+  if (step > 0) {
+    for (let i = start; i <= end; i += step) {
+      array.push(i);
+    }
+
+  } else {
+    for (var i = start; i >= end; i += step)
+      array.push(i);
+  }
+  return array;
+}
+
+function sum(array) {
+  var total = 0;
+  for (let i = 0; i < array.length; i++) {
+    total += array[i];
+
+  }
+  return total;
+}
+
+
+function factorial(n) {
+  if (n == 0) {
+    return 1;
+  } else {
+    return factorial(n - 1) * n;
+  }
+}
+
+console.log(factorial(8));//40320
